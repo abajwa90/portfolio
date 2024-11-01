95559ea446f00410a247e0c622276627164e68f3
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-01 12:15:26 -0400

    initial commit

---
+++ constants/data.js
@@ -0,0 +1,50 @@
+const userData = {
+  githubUsername: "abajwa90",
+  name: "Ameesa Bajwa",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "ameesabajwa@gmail.com",
+  address: "Toronto, Ontario, Canada.",
+  projects: [
+    {
+      title: "LumiCall",
+      link: "https://www.lumicall.dev/",
+      imgUrl: "/lumicall.png",
+    },
+    {
+      title: "Flora and Fern",
+      link: "https://flora-and-fern.vercel.app/",
+      imgUrl: "/florandfern.png",
+    },
+    {
+      title: "Agile Sync",
+      link: "https://agile-sync-react-7i680dqpk-abajwa90s-projects.vercel.app/",
+      imgUrl: "/agilesync.png",
+    },
+    {
+      title: "Portfolio",
+      link: "",
+      imgUrl: "/portfolio.png",
+    },
+  ],
+  about: {
+    title:
+      " Hey there! Thanks for stopping by :)",
+    description: [
+      `Hey there! Thanks for stopping by :)`,
+      `My name's Ameesa, and I'm a full-stack developer. I love making cool things on the internet, but I love when those cool things make a positive difference in people's lives more.`,
+      `When I'm not glued to my laptop, I'm usually making another cup of coffee or starting another research hobby (hello, Wikipedia).`,
+      `I believe that technology has the ability to make a tangible and positive difference in people's lives, and I aim to explore that aspect in my work.`,
+      `Feel free to reach out if you think we could work together!`
+    ],
+  },
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    linkedin: "https://linkedin.com/in/ameesa-bajwa",
+    github: "https://github.com/abajwa90",
+    medium: "https://medium.com/@ameesabajwa"
+  },
+};
+
+export default userData;
