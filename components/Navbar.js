4ddae6d439b3f5f9f2cabb6de3a8810caae54847
Ameesa Bajwa <ameesabajwa@gmail.com>
2024-11-04 02:14:34 -0500

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -37,22 +37,6 @@ export default function Navbar() {
                 : "text-gray-600 dark:text-gray-300 font-normal "
             }`}
           >
-            {/* About{" "}
-            {router.asPath === "/about" && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-arrow-down inline-block h-3 w-3"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                />
-              </svg>
-            )} */}
           </Link>
           <Link
             href="/projects"
@@ -78,31 +62,6 @@ export default function Navbar() {
                 />
               </svg>
             )}
-          </Link>
-          <Link
-            href="/experience"
-            className={`text-base  ${
-              router.asPath === "/experience"
-                ? "text-gray-800 font-bold dark:text-gray-400"
-                : "text-gray-600 dark:text-gray-300 font-normal "
-            }`}
-          >
-            {/* Experience{" "}
-            {router.asPath === "/experience" && (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-arrow-down inline-block h-3 w-3"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 1a.5.5 0 0 1 .5.5v11.793l3.146-3.147a.5.5 0 0 1 .708.708l-4 4a.5.5 0 0 1-.708 0l-4-4a.5.5 0 0 1 .708-.708L7.5 13.293V1.5A.5.5 0 0 1 8 1z"
-                />
-              </svg>
-            )} */}
           </Link>
           <Link
             href="/contact"
@@ -195,15 +154,9 @@ export default function Navbar() {
           Projects
         </Link>
         <Link
-          href="/experience"
-          className="text-base font-normal text-gray-600 dark:text-gray-300"
-        >
-          {/* Experience
-        </Link>
-        <Link
           href="/contact"
           className="text-base font-normal text-gray-600 dark:text-gray-300"
-        > */}
+        >
           Contact
         </Link>
       </div>
